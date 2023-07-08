67a87d4698c33220d123cb5afc1cb448e6700f83
FluffyAssassin <thea.lindgaard@gmail.com>
2023-07-09 00:32:50 +0200

    Quick script setup

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject levelSelect;
+    public GameObject credits;
+    public GameObject controls;
+
+    public void SwitchMenu(GameObject menu)
+    {
+        //all menus off
+        mainMenu.SetActive(false);
+        levelSelect.SetActive(false);
+        credits.SetActive(false);
+        controls.SetActive(false);
+
+        //current menu on
+        menu.SetActive(true);
+    }
+}
