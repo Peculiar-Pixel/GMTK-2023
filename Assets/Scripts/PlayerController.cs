c2f53a4bd84d8f06e984c2e44b7388cd61006d09
FluffyAssassin <thea.lindgaard@gmail.com>
2023-07-08 13:21:28 +0200

    Basic character controller working

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed = 1;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        if (rb == null) Debug.Log("Player rigidbody missing");
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        rb.velocity = new Vector3(horizontal, vertical, 0) * Time.deltaTime * speed;
+    }
+}
